76cdc3789f6d5d4d67174c04af86b663e38234db
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-07-02 14:35:52 -0400

    estilisando detalhes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/utils/app_routes.dart';
 import '/screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
-import 'screens/meal_detail.dart';
+import 'screens/meal_detail_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
       title: 'DeliMeals',
       theme: ThemeData(
         primaryColor: Colors.pink,
-        primarySwatch: Colors.pink,
         hintColor: Colors.amber,
         fontFamily: 'Raleway',
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
@@ -29,7 +28,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => const CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL: (ctx) => const MealDetail(),
+        AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/alguma-coisa') {
